1a094bb5d212431fa83d0baba0e4c450dee2779f
Jon Morgan <jmorjsm@gmail.com>
2021-12-11 11:02:09 +0000

    fix git config replacements

--- git/install.ps1
+++ git/install.ps1
@@ -1,10 +1,12 @@
 $gitConfigFileName = ".gitconfig"
-$gitConfigOutputFileName = "$Env:USERPROFILE\$gitConfigFileName"
+$gitConfigOutputFileName = Join-Path $Env:USERPROFILE $gitConfigFileName
 
-$gitConfigCoreExcludesFileName = "$Env:USERPROFILE\.gitignore"
+$gitConfigCoreExcludesFileName = Join-Path $Env:USERPROFILE ".gitignore"
 
 Write-Output "`tCopying .gitignore to user home directory."
 Copy-ItemWithBackup ".gitignore" $gitConfigCoreExcludesFileName
+
+$gitConfigCoreExcludesFileName = $gitConfigCoreExcludesFileName.Replace("\", "\\")
 
 $gitConfigUserEmail = Read-Host -Prompt "git config user email"
 $gitConfigUserName = Read-Host -Prompt "git config user name"
@@ -12,8 +14,5 @@ $gitConfigUserName = Read-Host -Prompt "git config user name"
 Write-Output "`tCopying .gitconfig to user home directory."
 
 New-FileBackup $gitConfigFileName $gitConfigOutputFileName
-((Get-Content -Path $gitConfigFileName -Raw) `
-    -replace '$(GitConfigCoreExcludesFileName)', $GitConfigCoreExcludesFileName `
-    -replace '$(GitConfigUserEmail)', $GitConfigUserEmail `
-    -replace '$(GitConfigUserName)', $GitConfigUserName) `
+((Get-Content -Path $gitConfigFileName -Raw).Replace('$(GitConfigCoreExcludesFileName)', $gitConfigCoreExcludesFileName).Replace('$(GitConfigUserEmail)', $gitConfigUserEmail).Replace('$(GitConfigUserName)', $gitConfigUserName)) `
     | Set-Content -path $gitConfigOutputFileName
