146573cf14e22d0aa2c2fb717ac4b08213b232eb
Jon Morgan <jmorjsm@gmail.com>
2023-03-13 21:15:28 +0000

    support windows terminal non-preview

--- windows-terminal/install.ps1
+++ windows-terminal/install.ps1
@@ -1,2 +1,13 @@
 Write-Output "`tCopying settings.json to Windows Terminal AppData folder..."
-Copy-ItemWithBackup "settings.json" "$env:UserProfile\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+
+$previewSettingsPath = "$env:UserProfile\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+if(test-path $previewSettingsPath)
+{
+    Copy-ItemWithBackup "settings.json" $previewSettingsPath
+}
+
+$mainSettingsPath = "$env:UserProfile\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+if(test-path $mainSettingsPath)
+{
+    Copy-ItemWithBackup "settings.json" $mainSettingsPath
+}
